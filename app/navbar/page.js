2d442b1c4515ba60f8621a693602393e5efa7125
Jeremy Myers <echelonical@gmail.com>
2023-10-12 21:44:46 -0500

    initial navbar layout

---
+++ app/navbar/page.js
@@ -0,0 +1,21 @@
+export default function NavBar() {
+    return (
+        <>
+            <nav className="flex flex-row p-1 border">
+                <div className="flex justify-center w-1/3 border">
+                    <div className="border">
+                        <h2>LOGO HERE</h2>
+                    </div>
+                </div>
+                <div className="flex justify-center w-1/3 border">
+                    <div className="uppercase">
+                        <h1>Welcome to Smooth Operator</h1>
+                    </div>
+                </div>
+                <div className="w-1/3 border">
+
+                </div>
+            </nav>
+        </>
+    );
+}
