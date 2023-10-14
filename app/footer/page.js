635d6bea07ae1e48d35da9663fae32e751b7ffbc
Jeremy Myers <echelonical@gmail.com>
2023-10-14 15:14:29 -0500

    adding footer layout

---
+++ app/footer/page.js
@@ -0,0 +1,17 @@
+export default function Footer() {
+
+    return (
+        <>
+            <footer className="absolute bottom-0 flex justify-center items-center h-36 w-full border">
+                <div className="flex justify-center w-6/12 border">
+                    <h2 className="h-16">Footer</h2>
+                </div>
+                <div className="w-6/12 border">
+
+                </div>
+
+
+            </footer>
+        </>
+    )
+}
