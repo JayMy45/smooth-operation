3c79e080d0f7e00257cb2e194caca8d287541ef5
Jeremy Myers <echelonical@gmail.com>
2023-10-15 14:36:54 -0500

    update links on navbar (name, hover, and referrences)

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NavBar() {
     return (
         <>
@@ -8,14 +10,18 @@ export default function NavBar() {
                     </div>
                 </div>
                 <div className="flex justify-around items-center w-6/12 border">
-                    <div className="uppercase">
-                        <h1>Welcome </h1>
+                    <div className="hover:border-b">
+                        <Link href="https://shopgofinity.com/?ref=LxaNoOARuhBx4eTS6E4tLcEYhCkfmpkDBXGf68Q1BPGm%2fOqPHE8Oc8IQg%2fxizZUld7rZjYWLIcemQgyAcOWs99XHW4xNMXjqNFpCTOyI57g%3d&prt=BAppufstuQJkjR41dAhfJtHdCd0BYJdU6CP%2fsqJgeibUjP%2f9pcTBp4Rj4ft0icF%2fh2LBGoyIrUYlOS3FqRhO75G6fYXEvTE6tXQ%2fu3tRFOU%3d">
+                            <h2 className="uppercase">gofinity</h2>
+                        </Link>
                     </div>
                     <div className="uppercase">
-                        <h2>to Smooth</h2>
+                        <h1>Smooth Operation</h1>
                     </div>
-                    <div className="uppercase">
-                        <h2>Operator</h2>
+                    <div className="hover:border-b">
+                        <Link href="https://1800386072.myasealive.com/">
+                            <h2 className="uppercase">asea</h2>
+                        </Link>
                     </div>
                 </div>
                 <div className="w-3/12 border">
