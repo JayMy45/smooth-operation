88bb8f6cb5cdc70ea321b9525051a89e3b410a69
Jeremy Myers <echelonical@gmail.com>
2023-10-15 12:35:43 -0500

    update product layout further to breakup paragraph.  This will not work whenever fetching data to map over

--- app/products/page.js
+++ app/products/page.js
@@ -24,20 +24,31 @@ export default function ProductsPage() {
                                 />
                             </div>
                             <div className=" w-fit border p-2 rounded-md mx-auto">
-                                <Link href="/products/1">
+                                <Link href="https://shopgofinity.com/products/skinny-gummies">
                                     <h3 className="text-xs">Click here to learn more</h3>
                                 </Link>
                             </div>
                         </div>
 
                         {/* header and description div */}
-                        <div className="w-4/6 h-full">
+                        <div className="w-4/6 h-full border">
                             <div className="p-2">
-                                <h1 className="text-xl font-bold">Products Name</h1>
+                                <header className="text-xl font-bold">Products Name</header>
                             </div>
 
                             <div className="mt-1 border h-16x">
-                                <p className="text-sm">Products Description</p>
+                                <p>Apple Cider Vinegar has been used for centuries as a traditional remedy to help support digestion, gut health, and appetite. With Wellfinity Skinny Gummies for adults, you can enjoy apple cider vinegar without the unpleasant taste. This delicious vegan formula is also free of caffeine, stimulants, and added sugars.</p>
+
+                                <p>- Helps to support and maintain a healthy metabolism.*</p>
+                                <p>- Vitamin B12 supports healthy nutrient metabolism*</p>
+                                <p>Wellfinity’ sweet, soft and chewy vegan gummies are made with fruit pectin and premium ingredients for great taste and results! Gelatin free.</p>
+
+                            </div>
+
+                            <div>
+                                <footer>
+                                    <h3>Awesome Divisions</h3>
+                                </footer>
                             </div>
                         </div>
 
