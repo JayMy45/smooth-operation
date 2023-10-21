59e4a11ca86858cae7e7de38806f20c633cdcfba
Jeremy Myers <echelonical@gmail.com>
2023-10-20 19:22:11 -0500

    working border

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -1,13 +1,30 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function NavBar() {
     const [productOpen, setProductOpen] = useState(false);
+    const dropdownRef = useRef(null);
 
     const handleToggle = () => {
-        setProductOpen(!navbarOpen);
+        setProductOpen(!productOpen);
     };
+
+    useEffect(() => {
+        const handleOutsideClick = (e) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+                setProductOpen(false);
+            }
+        };
+
+        window.addEventListener("click", handleOutsideClick);
+
+        return () => {
+            window.removeEventListener("click", handleOutsideClick);
+        };
+    }, []);
 
     return (
         <>
@@ -38,14 +55,30 @@ export default function NavBar() {
                         </Link>
                     </div>
                     <div className="flex justify-center items-center uppercase">
-                        <Link href="https://shopgofinity.com/?ref=LxaNoOARuhBx4eTS6E4tLcEYhCkfmpkDBXGf68Q1BPGm%2fOqPHE8Oc8IQg%2fxizZUld7rZjYWLIcemQgyAcOWs99XHW4xNMXjqNFpCTOyI57g%3d&prt=BAppufstuQJkjR41dAhfJtHdCd0BYJdU6CP%2fsqJgeibUjP%2f9pcTBp4Rj4ft0icF%2fh2LBGoyIrUYlOS3FqRhO75G6fYXEvTE6tXQ%2fu3tRFOU%3d">
+                        <Link href="https://1800386072.myasealive.com/">
                             <h2 className="uppercase">asea</h2>
                         </Link>
                     </div>
-                    <div className="hover:-b">
-                        <Link href="https://1800386072.myasealive.com/">
-                            <h2 className="uppercase">featured products</h2>
-                        </Link>
+                    <div className="flex justify-around items-center w-6/12 ">
+                        {/* ... other components ... */}
+                        <div className="hover:-b relative" ref={dropdownRef}>
+                            <div onClick={handleToggle}>
+                                Featured Products
+                            </div>
+                            {productOpen && (
+                                <div className="absolute top-full left-0 mt-2 flex flex-col justify-center items-center border w-fit h-fit px-5 bg-gray-100 dark:bg-gray-800 dark:border-gray-600">
+                                    <div>
+                                        Product 1
+                                    </div>
+                                    <div>
+                                        Product 2
+                                    </div>
+                                    <div>
+                                        Product 3
+                                    </div>
+                                </div>
+                            )}
+                        </div>
                     </div>
 
                 </div>
