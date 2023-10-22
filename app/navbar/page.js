d73108ca165ac77a6ceea4dd839a7aa782e6f014
Jeremy Myers <echelonical@gmail.com>
2023-10-22 11:27:11 -0500

    update navbar to show up/down arrow depending of productOpen state (true/false)

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { ProductList } from "../data/ProductList";
+import { BiSolidDownArrow, BiSolidUpArrow } from 'react-icons/bi'
 
 export default function NavBar() {
     const [productOpen, setProductOpen] = useState(false);
@@ -60,27 +61,40 @@ export default function NavBar() {
                             <h2 className="uppercase">asea</h2>
                         </Link>
                     </div>
-                    <div className="flex justify-around items-center w-fit ">
-                        <div className="hover:-b relative" ref={dropdownRef}>
-                            <div onClick={handleToggle} className="hover:border-b">
-                                Featured Products
-                            </div>
-                            {productOpen && (
-                                <div className="absolute top-full left-0 mt-2 flex flex-col justify-center items-center border w-fit h-fit p-2 bg-gray-800 dark:bg-inherent dark:border-gray-600 text-left">
-
-                                    {
-                                        ProductList.map(({ id, name, link }) => (
-                                            <div key={id} className="text-left w-full">
-                                                <Link href={link.href}>
-                                                    <h2 className="">{name}</h2>
-                                                </Link>
-                                            </div>
-                                        ))
-                                    }
-                                </div>
-                            )}
+                    <div className="hover:text-indigo-400 relative" ref={dropdownRef}>
+                        <div onClick={handleToggle} className="flex justify-between items-center">
+                            {productOpen
+                                ? <>
+                                    <h2 className="uppercase">Featured Products</h2>
+                                    <div className="ml-2">
+                                        <BiSolidUpArrow />
+                                    </div></>
+                                : <>
+                                    <h2 className="uppercase">Featured Products</h2>
+                                    <div className="ml-2">
+                                        <BiSolidDownArrow />
+                                    </div>
+                                </>
+                            }
                         </div>
+                        {productOpen && (
+                            <div
+                                className="absolute w-full transition ease-in duration-1000 bg-white dark:bg-gray-800 dark:text-white p-1 z-10 rounded-b-lg border-t"
+                                style={{ top: productOpen ? '100%' : '0%', transitionProperty: 'top' }}
+                            >
+                                {
+                                    ProductList.map(({ id, name, link }) => (
+                                        <div key={id} className="text-left ml-3 w-full p-1">
+                                            <Link href={link.href}>
+                                                <h2 className="">{name}</h2>
+                                            </Link>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        )}
                     </div>
+
 
                 </div>
                 <div className="w-3/12 ">
