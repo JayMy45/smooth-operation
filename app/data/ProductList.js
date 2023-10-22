05e5e4c86c79867963f095a45c5a8e3169760b98
Jeremy Myers <echelonical@gmail.com>
2023-10-22 13:02:14 -0500

    update ProductList with breakpoints <br> in description to provide display

--- app/data/ProductList.js
+++ app/data/ProductList.js
@@ -5,32 +5,25 @@ export const ProductList = [
         image: "/REDOXEnergy.png",
         alt: "REDOX Energy Image",
         link: "https://shop.aseaglobal.com/products/4313?sponsorId=18003HF07B&m=7&st=ps",
-        description: `REDOXEnergy leverages select Powered by Redox™ ingredients to provide a long-lasting source of cellular energy you can count on—whether it’s at home, work, or everything in between. As a healthy source of sustained cellular energy, this proprietary blend of B vitamins, Guarana, Ginseng, and L-Theanine help combat physical and mental fatigue. REDOXEnergy not only increases your output, but helps you generate and maintain more energy at the cellular level.`,
+        description: `REDOXEnergy leverages select Powered by Redox™ ingredients to provide a long-lasting source of cellular energy you can count on—whether it’s at home, work, or everything in between. As a healthy source of sustained cellular energy, this proprietary blend of B vitamins, Guarana, Ginseng, and L-Theanine help combat physical and mental fatigue. REDOXEnergy not only increases your output, but helps you generate and maintain more energy at the cellular level.`
     },
     {
         id: 2,
-        name: "Scalp Elixir",
-        image: "/ScalpElixir.png",
-        alt: "Scalp Elixir Image",
-        link: "https://shopgofinity.com/products/scalp-stimulating-elixir",
-        description: `We all know that one of the keys to healthy hair is a healthy scalp. And the keys to a healthy scalp include stimulating blood circulation which in turn stimulates hair follicles to do their job and do it well, i.e. to live a long life and multiply. Your keys are here.
-
-Our Scalp Stimulating Elixir delivers an intense infusion of vitamins and active botanicals to nourish targeted areas and support healthy hair growth. Use daily to revitalize the delicate, weak and overworked areas of your hair.
-
-Like all Hairfinity Elite products, this powerful, science-backed formula is clean, green and cruelty-free, formulated without sulfates, silicones, parabens, phthalates, mineral oil or petroleum. It’s safe for everyone and for all hair types, including hair that’s been colored, permed or otherwise processed.`,
-    },
-    {
-        id: 3,
         name: "Skinny Gummies",
         image: "/SkinnyGummies.png",
         alt: "Skinny Gummies Image",
         link: "https://shopgofinity.com/products/skinny-gummies",
-        description: `Apple Cider Vinegar has been used for centuries as a traditional remedy to help support digestion, gut health, and appetite. With Wellfinity Skinny Gummies for adults, you can enjoy apple cider vinegar without the unpleasant taste. This delicious vegan formula is also free of caffeine, stimulants, and added sugars.
-
-- Helps to support and maintain a healthy metabolism.*
-- Vitamin B12 supports healthy nutrient metabolism*
-Wellfinity’ sweet, soft and chewy vegan gummies are made with fruit pectin and premium ingredients for great taste and results! Gelatin free.`,
+        description: `Apple Cider Vinegar has been used for centuries as a traditional remedy to help support digestion, gut health, and appetite. With Wellfinity Skinny Gummies for adults, you can enjoy apple cider vinegar without the unpleasant taste. This delicious vegan formula is also free of caffeine, stimulants, and added sugars.<br /><br />- Helps to support and maintain a healthy metabolism.*<br /><br />- Vitamin B12 supports healthy nutrient metabolism*<br /><br />Wellfinity’ sweet, soft and chewy vegan gummies are made with fruit pectin and premium ingredients for great taste and results! Gelatin free.`
     },
+    {
+        id: 3,
+        name: "Scalp Elixir",
+        image: "/ScalpElixir.png",
+        alt: "Scalp Elixir Image",
+        link: "https://shopgofinity.com/products/scalp-stimulating-elixir",
+        description: `We all know that one of the keys to healthy hair is a healthy scalp. And the keys to a healthy scalp include stimulating blood circulation which in turn stimulates hair follicles to do their job and do it well, i.e. to live a long life and multiply. Your keys are here.<br /><br />Our Scalp Stimulating Elixir delivers an intense infusion of vitamins and active botanicals to nourish targeted areas and support healthy hair growth. Use daily to revitalize the delicate, weak and overworked areas of your hair.<br /><br />Like all Hairfinity Elite products, this powerful, science-backed formula is clean, green and cruelty-free, formulated without sulfates, silicones, parabens, phthalates, mineral oil or petroleum. It’s safe for everyone and for all hair types, including hair that’s been colored, permed or otherwise processed.`
+    },
+
     {
         id: 4,
         name: "ASEA Redox",
@@ -38,5 +31,5 @@ Wellfinity’ sweet, soft and chewy vegan gummies are made with fruit pectin and
         alt: "ASEA Redox Image",
         link: "https://1800386072.myasealive.com/",
         description: `ASEA has developed a unique breakthrough product technology that has been scientifically tested and shown to signal the activation of genetic pathways or affect genes that:`
-    },
+    }
 ];
