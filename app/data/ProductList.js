b4f4c47ce0a28c44e2ce9f1e9e0ff07157291301
Jeremy Myers <echelonical@gmail.com>
2023-10-21 20:37:13 -0500

    created ProductList array as a place to store product data

---
+++ app/data/ProductList.js
@@ -0,0 +1,87 @@
+export const ProductList = [
+    {
+        id: 1,
+        name: "Skinny Gummies",
+        image: {
+            src: "/SkinnyGummies.png",
+            alt: "Vercel Logo",
+            width: 100,
+            height: 100,
+            className: "",
+            priority: true,
+        },
+        link: {
+            href: "https://shopgofinity.com/products/skinny-gummies",
+            text: "Click here to learn more",
+        },
+        header: {
+            text: "Skinny Gummies",
+            className: "text-xl font-bold uppercase",
+        },
+        description: `Apple Cider Vinegar has been used for centuries as a traditional remedy to help support digestion, gut health, and appetite. With Wellfinity Skinny Gummies for adults, you can enjoy apple cider vinegar without the unpleasant taste. This delicious vegan formula is also free of caffeine, stimulants, and added sugars.
+
+- Helps to support and maintain a healthy metabolism.*
+- Vitamin B12 supports healthy nutrient metabolism*
+Wellfinity’ sweet, soft and chewy vegan gummies are made with fruit pectin and premium ingredients for great taste and results! Gelatin free.`,
+        footer: {
+            text: "Awesome Divisions",
+            className: "italic text-xs"
+        }
+    },
+    {
+        id: 2,
+        name: "Scalp Elixir",
+        image: {
+            src: "/ScalpElixir.png",
+            alt: "Vercel Logo",
+            width: 100,
+            height: 100,
+            className: "",
+            priority: true,
+        },
+        link: {
+            href: "https://shopgofinity.com/products/scalp-stimulating-elixir",
+            text: "Click here to learn more",
+        },
+        header: {
+            text: "Scalp Elixir",
+            className: "text-xl font-bold uppercase",
+        },
+        description: `We all know that one of the keys to healthy hair is a healthy scalp. And the keys to a healthy scalp include stimulating blood circulation which in turn stimulates hair follicles to do their job and do it well, i.e. to live a long life and multiply. Your keys are here.
+
+Our Scalp Stimulating Elixir delivers an intense infusion of vitamins and active botanicals to nourish targeted areas and support healthy hair growth. Use daily to revitalize the delicate, weak and overworked areas of your hair.
+
+Like all Hairfinity Elite products, this powerful, science-backed formula is clean, green and cruelty-free, formulated without sulfates, silicones, parabens, phthalates, mineral oil or petroleum. It’s safe for everyone and for all hair types, including hair that’s been colored, permed or otherwise processed.`,
+        footer: {
+            text: "Awesome Divisions",
+            className: "italic text-xs"
+        }
+    },
+    {
+        id: 3,
+        name: "REDOXEnergy",
+        image: {
+            src: "/ASEARedox.png",
+            alt: "REDOX Energy Image",
+            width: 100,
+            height: 100,
+            className: "",
+            priority: true,
+        },
+        link: {
+            href: "https://shopgofinity.com/products/scalp-stimulating-elixir",
+            text: "Click here to learn more",
+        },
+        header: {
+            text: "REDOXEnergy",
+            className: "text-xl font-bold",
+        },
+        description: `ASEA has developed a unique breakthrough product technology
+that has been scientifically tested and shown to signal the
+activation of genetic pathways or affect genes that:`,
+        footer: {
+            text: "Awesome Divisions",
+            className: "italic text-xs"
+        }
+    }
+];
