7fa2062cb1ba41766194784a1d26085ee462f9de
Jeremy Myers <echelonical@gmail.com>
2023-10-23 12:35:59 -0500

    update navbar with allProducts link

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -75,10 +75,15 @@ export default function NavBar() {
                             <div
                                 className="absolute w-full bg-white dark:bg-gray-800 dark:text-white text-gray-800 p-1 z-10 rounded-b-lg border-t"
                             >
+                                <div className="text-left ml-3 w-full p-1 hover:text-lg hover:text-indigo-800 dark:hover:text-gray-300 hover:font-bold">
+                                    <Link href={`/products`}>
+                                        <h2 className="font-bold">All Products</h2>
+                                    </Link>
+                                </div>
                                 {
                                     ProductList.map(({ id, name, link }) => (
                                         <div key={id} className="text-left ml-3 w-full p-1 hover:text-lg hover:text-indigo-800 dark:hover:text-gray-300 hover:font-bold">
-                                            <Link href={link}>
+                                            <Link href={`/products/${id}`}>
                                                 <h2 className="">{name}</h2>
                                             </Link>
                                         </div>
