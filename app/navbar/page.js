a8c42f6e0d168f20518a53482ff66fb2927e58ae
Jeremy Myers <echelonical@gmail.com>
2023-10-24 20:11:03 -0500

    update original navbar styling

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -31,79 +31,81 @@ export default function NavBar() {
 
     return (
         <>
-            <nav className="hidden md:block relative top-0 flex flex-row h-32 bg-black text-white h-fit sticky z-10">
-                <div className="flex justify-center items-center w-3/12 ">
+            <nav className="hidden md:block relative top-0 h-32 bg-black text-white h-fit sticky z-10">
+                <div className="flex flex-row ">
+                    <div className="flex justify-center items-center w-3/12 ">
 
-                    <div className="uppercase">
-                        <Image
-                            src="/smoothlogo1.png"
-                            alt="Smooth Operations Logo"
-                            className="bg-black"
-                            width={200}
-                            height={200}
-                        />
-                    </div>
-                </div>
-                <div className="flex justify-around items-center w-6/12 ">
-                    <div className="hover:-b">
-                        <Link href="https://shopgofinity.com/?ref=LxaNoOARuhBx4eTS6E4tLcEYhCkfmpkDBXGf68Q1BPGm%2fOqPHE8Oc8IQg%2fxizZUld7rZjYWLIcemQgyAcOWs99XHW4xNMXjqNFpCTOyI57g%3d&prt=BAppufstuQJkjR41dAhfJtHdCd0BYJdU6CP%2fsqJgeibUjP%2f9pcTBp4Rj4ft0icF%2fh2LBGoyIrUYlOS3FqRhO75G6fYXEvTE6tXQ%2fu3tRFOU%3d">
-                            <h2 className="uppercase">gofinity</h2>
-                        </Link>
-                    </div>
-                    <div className="flex justify-center items-center uppercase">
-                        <Link href="https://1800386072.myasealive.com/">
-                            <h2 className="uppercase">asea</h2>
-                        </Link>
-                    </div>
-                    <div className="hover:text-indigo-400 relative" ref={dropdownRef}>
-                        <div onClick={handleToggle} className="flex justify-between items-center cursor-pointer">
-                            {productOpen
-                                ? <>
-                                    <div className="">
-                                        <h2 className="uppercase">Featured Products</h2>
-                                    </div>
-                                    <BiSolidUpArrow className="ml-2" />
-                                </>
-                                : <>
-                                    <div className="">
-                                        <h2 className="uppercase">Featured Products</h2>
-                                    </div>
-                                    <BiSolidDownArrow className="ml-2" />
-                                </>
-                            }
+                        <div className="uppercase">
+                            <Image
+                                src="/smoothlogo1.png"
+                                alt="Smooth Operations Logo"
+                                className="bg-black"
+                                width={200}
+                                height={200}
+                            />
                         </div>
-                        {productOpen && (
-                            <div
-                                className="absolute w-full bg-white dark:bg-gray-800 dark:text-white text-gray-800 p-1 z-10 rounded-b-lg border-t"
-                            >
-                                <div className="text-left ml-3 w-full p-1 hover:text-lg hover:text-indigo-800 dark:hover:text-gray-300 hover:font-bold">
-                                    <Link href={`/`}
-                                        onClick={handleToggle}
-                                    >
-                                        <h2 className="font-bold">All Products</h2>
-                                    </Link>
-                                </div>
-                                {
-                                    ProductList.map(({ id, name, link }) => (
-                                        <div key={id} className="text-left ml-3 w-full p-1 hover:text-lg hover:text-indigo-800 dark:hover:text-gray-300 hover:font-bold">
-                                            <Link href={`/products/${id}`}
-                                                onClick={handleToggle}
-                                            >
-                                                <h2 className="">{name}</h2>
-                                            </Link>
+                    </div>
+                    <div className="flex justify-around items-center w-6/12 ">
+                        <div className="hover:-b">
+                            <Link href="https://shopgofinity.com/?ref=LxaNoOARuhBx4eTS6E4tLcEYhCkfmpkDBXGf68Q1BPGm%2fOqPHE8Oc8IQg%2fxizZUld7rZjYWLIcemQgyAcOWs99XHW4xNMXjqNFpCTOyI57g%3d&prt=BAppufstuQJkjR41dAhfJtHdCd0BYJdU6CP%2fsqJgeibUjP%2f9pcTBp4Rj4ft0icF%2fh2LBGoyIrUYlOS3FqRhO75G6fYXEvTE6tXQ%2fu3tRFOU%3d">
+                                <h2 className="uppercase">gofinity</h2>
+                            </Link>
+                        </div>
+                        <div className="flex justify-center items-center uppercase">
+                            <Link href="https://1800386072.myasealive.com/">
+                                <h2 className="uppercase">asea</h2>
+                            </Link>
+                        </div>
+                        <div className="hover:text-indigo-400 relative" ref={dropdownRef}>
+                            <div onClick={handleToggle} className="flex justify-between items-center cursor-pointer">
+                                {productOpen
+                                    ? <>
+                                        <div className="">
+                                            <h2 className="uppercase">Featured Products</h2>
                                         </div>
-                                    ))
+                                        <BiSolidUpArrow className="ml-2" />
+                                    </>
+                                    : <>
+                                        <div className="">
+                                            <h2 className="uppercase">Featured Products</h2>
+                                        </div>
+                                        <BiSolidDownArrow className="ml-2" />
+                                    </>
                                 }
                             </div>
+                            {productOpen && (
+                                <div
+                                    className="absolute w-full bg-white dark:bg-gray-800 dark:text-white text-gray-800 p-1 z-10 rounded-b-lg border-t"
+                                >
+                                    <div className="text-left ml-3 w-full p-1 hover:text-lg hover:text-indigo-800 dark:hover:text-gray-300 hover:font-bold">
+                                        <Link href={`/`}
+                                            onClick={handleToggle}
+                                        >
+                                            <h2 className="font-bold">All Products</h2>
+                                        </Link>
+                                    </div>
+                                    {
+                                        ProductList.map(({ id, name, link }) => (
+                                            <div key={id} className="text-left ml-3 w-full p-1 hover:text-lg hover:text-indigo-800 dark:hover:text-gray-300 hover:font-bold">
+                                                <Link href={`/products/${id}`}
+                                                    onClick={handleToggle}
+                                                >
+                                                    <h2 className="">{name}</h2>
+                                                </Link>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
 
-                        )}
+                            )}
+                        </div>
+
+
                     </div>
+                    <div className="w-3/12 ">
 
 
-                </div>
-                <div className="w-3/12 ">
-
-
+                    </div>
                 </div>
             </nav>
 
