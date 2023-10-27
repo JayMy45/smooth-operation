fb2ddf31c066c7d041942033f024a8910dc0a170
Jeremy Myers <echelonical@gmail.com>
2023-10-27 18:58:08 -0500

    update details page for company to find company by id

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,19 +1,23 @@
 import Link from "next/link"
 import { CompanyList } from "../data/CompanyList"
 
-export default function CompanyPage() {
+export default function CompanyPage({ params }) {
+
+    const { id } = params;
+    const company = CompanyList.find((company) => company.id === Number(id));
+
     return (
         <>
             <section className="h-screen">
                 <div className="text-center border">
-                    {CompanyList.map(({ id, name, link, image, alt }) => (
-                        <div key={id}>
-                            <h1>{name}</h1>
-                            <Link href={link}>
-                                This is A Link
+                    <div>
+                        <h1>{company.name}</h1>
+                        <div className="border border-green-500">
+                            <Link href={company.link}>
+                                Visit the site for {company.name} here
                             </Link>
                         </div>
-                    ))}
+                    </div>
                 </div>
             </section>
         </>
