7d51b57c2e1a960d76cc46bf573d751523c47356
Jeremy Myers <echelonical@gmail.com>
2023-10-27 19:38:22 -0500

    update background color of company cards and updated layouts and functionality

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -9,12 +9,12 @@ export default function CompanyPage({ params }) {
     return (
         <>
             <section className="flex justify-center h-screen">
-                <div className="flex justify-around border rounded-xl h-1/2 w-1/2 p-2">
+                <div className={`flex justify-around ${company.bgColor} rounded-xl h-1/2 w-1/2 p-2 shadow-xl`}>
                     <div className="flex flex-col justify-between py-5">
                         <h1 className="uppercase text-5xl">{company.name}</h1>
-                        <div className="border border-green-500">
+                        <div className="border border-slate-200 p-2 text-center rounded-lg w-fit">
                             <Link href={company.link}>
-                                <h3 className="italic text-sm">To learn more about {company.name} click here</h3>
+                                <h3 className="italic text-sm text-gray-300">Go to {company.name} here</h3>
                             </Link>
                         </div>
                     </div>
