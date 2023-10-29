5ccf4fba14d3942ad33574ad07f66ea4da579d99
Jeremy Myers <echelonical@gmail.com>
2023-10-29 14:57:42 -0500

    update footer responsiveness/layout on small screens

--- app/footer/page.js
+++ app/footer/page.js
@@ -8,7 +8,7 @@ export default function Footer() {
     return (
         <>
             <footer className="flex justify-between items-center h-36 w-full px-7 bg-black">
-                <div className="flex justify-center items-center w-3/12">
+                <div className="flex justify-center items-center w-full md:w-3/12">
                     <div className="uppercase">
                         <Image
                             src="/smoothlogo2.png"
@@ -19,24 +19,26 @@ export default function Footer() {
                         />
                     </div>
                 </div>
-                <div className="flex justify-center w-3/12 h-full p-3">
-                    {
-                        <div>
-                            {
-                                ProductList.map(({ id, name, link }) => (
-                                    <div key={id} className="text-left ml-3 w-full p-1 hover:text-lg hover:text-indigo-800 dark:hover:text-gray-300 hover:font-bold">
-                                        <Link href={`/products/${id}`}
-                                        >
-                                            <h2 className="text-xs">{name}</h2>
-                                        </Link>
-                                    </div>
-                                ))
-                            }
-                        </div>
+                <div className="hidden md:block w-3/12 h-full p-3">
+                    <div className="flex justify-center">
+                        {
+                            <div className="hidden md:block">
+                                {
+                                    ProductList.map(({ id, name, link }) => (
+                                        <div key={id} className="text-left ml-3 w-full p-1 hover:text-lg hover:text-indigo-800 dark:hover:text-gray-300 hover:font-bold">
+                                            <Link href={`/products/${id}`}
+                                            >
+                                                <h2 className="text-xs">{name}</h2>
+                                            </Link>
+                                        </div>
+                                    ))
+                                }
+                            </div>
 
-                    }
+                        }
+                    </div>
                 </div>
-                <div className="w-6/12 border h-full">
+                <div className="hidden md:block w-6/12 border h-full">
 
                 </div>
 
