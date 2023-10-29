ad1a393c9a045de7d6b28e67be0c93ae6815e48d
Jeremy Myers <echelonical@gmail.com>
2023-10-29 14:49:30 -0500

    update small screen layout on company details site

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -8,13 +8,13 @@ export default function CompanyPage({ params }) {
 
     return (
         <>
-            <section className="flex justify-center h-screen">
-                <div className={`flex justify-around ${company.bgColor} rounded-xl h-1/2 w-1/2 p-2 shadow-xl`}>
+            <section className="flex justify-center h-[70vh] md:h-screen p-5">
+                <div className={`flex flex-col justify-around ${company.bgColor} rounded-xl px-5 shadow-xl`}>
                     <div className="flex flex-col justify-between py-5">
-                        <h1 className="uppercase text-5xl">{company.name}</h1>
+                        <h1 className="uppercase text-5xl mb-4">{company.name}</h1>
                         <div className="border border-slate-200 p-2 text-center rounded-lg w-fit">
                             <Link href={company.link}>
-                                <h3 className="italic text-sm text-gray-300">Go to {company.name} here</h3>
+                                <h3 className="italic text-sm text-gray-300">Visit {company.name}'site site here</h3>
                             </Link>
                         </div>
                     </div>
