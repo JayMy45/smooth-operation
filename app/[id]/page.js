4779a72c41c48f17f60574cc9c25bf7e13fa1481
Jeremy Myers <echelonical@gmail.com>
2023-10-30 20:11:40 -0500

    hide products on company details page for small screens

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -27,26 +27,28 @@ export default function CompanyPage({ params }) {
                             <p className="text-slate-200">{company.description}</p>
                         </div>
                     </div>
-                    <div className="flex justify-evenly mt-10">
-                        {
-                            ProductList.filter((product) => product.company === company.name).map(({ id, name, image, alt, link }) => (
-                                <Link href={link}>
-                                    <div className="hover:shadow-lg p-3">
-                                        <Image
-                                            src={image}
-                                            alt={alt}
-                                            width={200}
-                                            height={200}
-                                            className="rounded-full"
-                                        />
-                                        <div className="mt-3">
-                                            <h2 className="text-center text-slate-200 font-semibold">{name}</h2>
+                    <div className="hidden md:block">
+                        <div className="flex justify-evenly mt-10">
+                            {
+                                ProductList.filter((product) => product.company === company.name).map(({ id, name, image, alt, link }) => (
+                                    <Link href={link}>
+                                        <div className="hover:shadow-lg p-3">
+                                            <Image
+                                                src={image}
+                                                alt={alt}
+                                                width={200}
+                                                height={200}
+                                                className="rounded-full"
+                                            />
+                                            <div className="mt-3">
+                                                <h2 className="text-center text-slate-200 font-semibold">{name}</h2>
+                                            </div>
                                         </div>
-                                    </div>
-                                </Link>
+                                    </Link>
 
-                            ))
-                        }
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
             </section>
