e140e5a59189cea59a490d41145f7d127c2364aa
Jeremy Myers <echelonical@gmail.com>
2023-11-01 19:27:23 -0500

    404 page added

---
+++ app/not-found.js
@@ -0,0 +1,31 @@
+'use client'
+
+import React, { useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function NotFound() {
+    useEffect(() => {
+        setTimeout(() => {
+            window.location.href = '/';
+        }, 4000); // Redirect after 3 seconds
+    }, []);
+
+    return (
+        <div className='max-h-fit py-5 lg:px-32'>
+            <div className='flex justify-center mt-5'>
+                <Link href="/" className='font-bold text-xl hover:border-2'>You will be redirected to the homepage shortly</Link>
+            </div>
+            <div className='flex justify-center items-center mt-5'>
+                <Image
+                    src="/404Logo.png"
+                    alt="404 Logo"
+                    width={750}
+                    height={750}
+                    className='rounded-lg'
+                />
+            </div>
+            <h2 className='text-center text-xl font-semibold'>request Not Found</h2>
+        </div>
+    )
+}
