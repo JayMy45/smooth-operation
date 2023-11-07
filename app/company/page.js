c981c38e683861870b5ff280982acf78752c9293
Jeremy Myers <echelonical@gmail.com>
2023-11-06 19:40:05 -0600

    update company logo when hover to goto link

--- app/company/page.js
+++ app/company/page.js
@@ -6,23 +6,23 @@ export default function CompanyList() {
         <>
             <section className="h-full">
                 <div className="flex flex-row justify-center items-center space-x-2">
-                    <div>
+                    <div className="rounded-lg overflow-hidden hover:border hover:border-gray-800 hover:shadow-lg hover:shadow-gray-500">
                         <Link href="/company/1">
                             <Image
                                 src={"/ASEA.png"}
                                 height={400}
                                 width={400}
-                                className="bg-gray-300 rounded-lg"
+                                className="bg-gradient-to-r from-black via-blue-700 to-black"
                             />
                         </Link>
                     </div>
-                    <div>
+                    <div className="rounded-lg overflow-hidden hover:border hover:border-gray-800 hover:shadow-lg hover:shadow-gray-500">
                         <Link href="/company/2">
                             <Image
                                 src={"/GOFINITY.png"}
                                 height={400}
                                 width={400}
-                                className="bg-gray-300 rounded-lg"
+                                className="bg-gradient-to-r from-black via-pink-600 to-black"
                             />
                         </Link>
                     </div>
