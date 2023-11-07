1401fd973446995911f1416a5d466819edcd9eb9
Jeremy Myers <echelonical@gmail.com>
2023-11-06 19:48:32 -0600

    update company page

--- app/company/page.js
+++ app/company/page.js
@@ -5,11 +5,12 @@ export default function CompanyList() {
     return (
         <>
             <section className="h-full">
-                <div className="flex flex-row justify-center items-center space-x-2">
+                <div className="flex flex-col md:flex-row justify-center items-center space-x-2 space-y-5 md:space-y-0 mb-5">
                     <div className="rounded-lg overflow-hidden hover:border hover:border-gray-800 hover:shadow-lg hover:shadow-gray-500">
                         <Link href="/company/1">
                             <Image
                                 src={"/ASEA.png"}
+                                alt="ASEA Logo Image Link to company description page"
                                 height={400}
                                 width={400}
                                 className="bg-gradient-to-r from-black via-blue-700 to-black"
@@ -20,6 +21,7 @@ export default function CompanyList() {
                         <Link href="/company/2">
                             <Image
                                 src={"/GOFINITY.png"}
+                                alt="GOFINITY Logo Image Link to company description page"
                                 height={400}
                                 width={400}
                                 className="bg-gradient-to-r from-black via-pink-600 to-black"
