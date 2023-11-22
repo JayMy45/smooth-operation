e4436b61d46a5682963e1dd580980115372f97fb
Jeremy Myers <echelonical@gmail.com>
2023-11-22 15:01:37 -0500

    add Roboto condensed font to project.

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto_Condensed } from 'next/font/google'
 import NavBar from './navbar/page'
 import Footer from './footer/page'
 
 const inter = Inter({ subsets: ['latin'] })
+const robotoCondensed = Roboto_Condensed({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export const metadata = {
   title: 'Smooth Operations',
@@ -13,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} ${robotoCondensed.className}`}>
         <div className="flex flex-col min-h-screen">
           <NavBar />
           <div className="flex-grow"> {/* This div will expand and push the footer down */}
