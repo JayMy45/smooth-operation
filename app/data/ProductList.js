b96d07615754e7a565047d92ffe0f390b2838167
Jeremy Myers <echelonical@gmail.com>
2023-12-03 13:13:44 -0600

    update database with active property

--- app/data/ProductList.js
+++ app/data/ProductList.js
@@ -8,7 +8,8 @@ export const ProductList = [
         link: "https://shop.aseaglobal.com/products/4313?sponsorId=18003HF07B&m=7&st=ps",
         video: "https://1800386072.shareasea.com/presentation/video.asea-cell-performance-redox-mind-redox-mood-and-redox-energy?utm_campaign=asea&utm_medium=copy&utm_source=android&utm_content=video_5950&utm_term=asea-cell-performance-redox-mind-redox-mood-and-redox-energy&utm_distributor=1800386072",
         pdf: "",
-        description: `REDOXEnergy leverages select Powered by Redox™ ingredients to provide a long-lasting source of cellular energy you can count on—whether it’s at home, work, or everything in between. As a healthy source of sustained cellular energy, this proprietary blend of B vitamins, Guarana, Ginseng, and L-Theanine help combat physical and mental fatigue. REDOXEnergy not only increases your output, but helps you generate and maintain more energy at the cellular level.`
+        description: `REDOXEnergy leverages select Powered by Redox™ ingredients to provide a long-lasting source of cellular energy you can count on—whether it’s at home, work, or everything in between. As a healthy source of sustained cellular energy, this proprietary blend of B vitamins, Guarana, Ginseng, and L-Theanine help combat physical and mental fatigue. REDOXEnergy not only increases your output, but helps you generate and maintain more energy at the cellular level.`,
+        active: false
     },
     {
         id: 2,
@@ -19,7 +20,8 @@ export const ProductList = [
         link: "https://shopgofinity.com/products/skinny-gummies",
         video: "",
         pdf: "",
-        description: `Apple Cider Vinegar has been used for centuries as a traditional remedy to help support digestion, gut health, and appetite. With Wellfinity Skinny Gummies for adults, you can enjoy apple cider vinegar without the unpleasant taste. This delicious vegan formula is also free of caffeine, stimulants, and added sugars.<br /><br />- Helps to support and maintain a healthy metabolism.*<br /><br />- Vitamin B12 supports healthy nutrient metabolism*<br /><br />Wellfinity’ sweet, soft and chewy vegan gummies are made with fruit pectin and premium ingredients for great taste and results! Gelatin free.`
+        description: `Apple Cider Vinegar has been used for centuries as a traditional remedy to help support digestion, gut health, and appetite. With Wellfinity Skinny Gummies for adults, you can enjoy apple cider vinegar without the unpleasant taste. This delicious vegan formula is also free of caffeine, stimulants, and added sugars.<br /><br />- Helps to support and maintain a healthy metabolism.*<br /><br />- Vitamin B12 supports healthy nutrient metabolism*<br /><br />Wellfinity’ sweet, soft and chewy vegan gummies are made with fruit pectin and premium ingredients for great taste and results! Gelatin free.`,
+        active: true
     },
     {
         id: 3,
@@ -30,7 +32,8 @@ export const ProductList = [
         link: "https://shopgofinity.com/products/scalp-stimulating-elixir",
         video: "",
         pdf: "",
-        description: `The keys to a healthy scalp include stimulating blood circulation which in turn stimulates hair follicles to do their job well. <br /><br />Our Scalp Stimulating Elixir delivers an intense infusion of vitamins and active botanicals to nourish targeted areas and support healthy hair growth. Use daily to revitalize the delicate, weak and overworked areas of your hair.<br /><br />Like all Hairfinity Elite products, this powerful, science-backed formula is clean, green and cruelty-free, formulated without sulfates, silicones, parabens, phthalates, mineral oil or petroleum.`
+        description: `The keys to a healthy scalp include stimulating blood circulation which in turn stimulates hair follicles to do their job well. <br /><br />Our Scalp Stimulating Elixir delivers an intense infusion of vitamins and active botanicals to nourish targeted areas and support healthy hair growth. Use daily to revitalize the delicate, weak and overworked areas of your hair.<br /><br />Like all Hairfinity Elite products, this powerful, science-backed formula is clean, green and cruelty-free, formulated without sulfates, silicones, parabens, phthalates, mineral oil or petroleum.`,
+        active: true
     },
 
     {
@@ -42,7 +45,8 @@ export const ProductList = [
         link: "https://shop.aseaglobal.com/products/2365?sponsorId=18003HF07B&m=7&st=ps",
         video: "https://1800386072.shareasea.com/presentation/video.asea-redox-transformational-technology-1?utm_campaign=asea&utm_medium=copy&utm_source=android&utm_content=video_5536&utm_term=asea-redox-transformational-technology-1&utm_distributor=1800386072",
         pdf: "/ASEA_REDOXBookletEN.pdf",
-        description: `ASEA has developed a unique breakthrough product technology that has been scientifically tested and shown to signal the activation of genetic pathways or affect genes that:`
+        description: `ASEA has developed a unique breakthrough product technology that has been scientifically tested and shown to signal the activation of genetic pathways or affect genes that:`,
+        active: true
     },
     {
         id: 5,
@@ -53,6 +57,19 @@ export const ProductList = [
         link: "https://shop.aseaglobal.com/products/4850?sponsorId=18003HF07B&m=7&st=ps",
         video: "",
         pdf: "",
-        description: `Draw out the day’s impurities and enjoy healthy surface skin cell renewal with ASEA® Redox Clay Mask. This blend of bentonite clay and active redox signaling molecules absorbs excess oils, tightens the skin, and reduces the appearance of enlarged pores.`
+        description: `Draw out the day’s impurities and enjoy healthy surface skin cell renewal with ASEA® Redox Clay Mask. This blend of bentonite clay and active redox signaling molecules absorbs excess oils, tightens the skin, and reduces the appearance of enlarged pores.`,
+        active: false
     },
+    {
+        id: 6,
+        name: "Skin Care System",
+        company: "ASEA",
+        image: "/SkinCare.png",
+        alt: "ASEA Redox Image",
+        link: "https://shop.aseaglobal.com/products/4850?sponsorId=18003HF07B&m=7&st=ps",
+        video: "",
+        pdf: "",
+        description: `The RENUAdvanced Skin Care System by ASEA is designed to rejuvenate and renew skin, harnessing the power of redox principles. It aims to maintain skin's natural redox balance, leading to healthier, more vibrant skin. The four-step regimen includes products like a foaming cleanser, balancing toner, glow serum, and hydrating cream. Key ingredients include beet root extract for hydration, Leontopodium alpinum extract for soothing, and other natural extracts for skin health. The system is free from harmful chemicals, gluten-free, fragrance-free, and cruelty-free. RENU28, an optional add-on, further enhances skin care by reducing cellulite and wrinkles.`,
+        active: true
+    }
 ];
